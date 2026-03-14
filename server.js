7b62b26d7324abbe648b4722a185538649c047e6
Your Name <you@example.com>
2026-03-14 14:02:14 +0530

    chore: add .gitignore, load dotenv in server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -5,7 +7,7 @@ const path = require('path');
 const Product = require('./models/Product');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
